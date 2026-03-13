c946d9858ad55a723736ff02076e2beb4573ccaa
King-Paolo <gi7pa5@live.it>
2026-03-13 21:17:05 +0100

    Aggiunto limite dash e Coins

--- Assets/_Project/Paolo/Scripts/PlayerController.cs
+++ Assets/_Project/Paolo/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public enum CurrentLane { LEFTLANE, RIGHTLANE, MIDLANE }
+
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _jumpForce;
-    [SerializeField] private float _addGravity;
+    //[SerializeField] private float _addGravity;
     [SerializeField] private float _dashForce = 10f;
     [SerializeField] private float _dashDuration = 0.2f;
 
@@ -15,6 +17,10 @@ public class PlayerController : MonoBehaviour
     private bool _isDashing = false;
     private float _dashTime = 0f;
     private float _direction;
+    private CurrentLane _currentLane;
+    private int _cyberScore;
+    private int _naturalScore;
+    private int _blackWhiteScore;
 
     private void Awake()
     {
@@ -27,12 +33,25 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && _gc.IsGrounded)
             Jump();
 
+        if (transform.position.x < 0)
+        {
+            _currentLane = CurrentLane.LEFTLANE;
+        }
+        else if (transform.position.x > 0)
+        {
+            _currentLane = CurrentLane.RIGHTLANE;
+        }
+        else if (transform.position.x == 0)
+        {
+            _currentLane = CurrentLane.MIDLANE;
+        }
+
         if (!_isDashing)
         {
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Input.GetKeyDown(KeyCode.A) && _currentLane != CurrentLane.LEFTLANE)
                 Dash(-1);
 
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.D) && _currentLane != CurrentLane.RIGHTLANE)
                 Dash(1);
         }
     }
@@ -49,15 +68,23 @@ public class PlayerController : MonoBehaviour
 
             if (_dashTime >= _dashDuration)
             {
-                _isDashing = false; 
+                _isDashing = false;
             }
         }
 
         _rb.velocity = new Vector3(horizontal.x, _rb.velocity.y, horizontal.z);
 
-        if (_rb.velocity.y < 0)
+        //if (_rb.velocity.y < 0)
+        //{
+        //    _rb.AddForce(Vector3.down * _addGravity);
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent<Coins>(out var coins))
         {
-            _rb.AddForce(Vector3.down * _addGravity);
+            coins.Collect(this);
         }
     }
 
@@ -71,6 +98,23 @@ public class PlayerController : MonoBehaviour
         _isDashing = true;
         _direction = direction;
         _dashTime = 0f;
+    }
+
+    public void AddScore(int amount, Coins.coinType coin)
+    {
+        switch (coin)
+        {
+            case Coins.coinType.CYBERCOIN:
+                _cyberScore += amount;
+                break;
+            case Coins.coinType.NATURALCOIN:
+                _naturalScore += amount;
+                break;
+            case Coins.coinType.BLACKWHITECOIN:
+                _blackWhiteScore += amount;
+                break;
+        }
+        Debug.Log("raccolto" + coin + _cyberScore + _naturalScore + _blackWhiteScore);
     }
 }
 
