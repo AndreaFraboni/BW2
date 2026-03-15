5d838aec3b2e09395c3a4dd01a1c5e5d111d2d1c
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-03-15 09:40:29 +0100

    Update to TileSpwaner

--- Assets/_Project/Andrea/Scripts/TileSpawner.cs
+++ Assets/_Project/Andrea/Scripts/TileSpawner.cs
@@ -18,8 +18,10 @@ public class TileSpawner : MonoBehaviour
     [SerializeField] private GameObject _StartingTileGO;
     [SerializeField] private Vector3 _resetPos = new Vector3(0f, 1f, -50f);
 
-    [SerializeField] private GameObject _currentTile;
     [SerializeField] private GameObject _startingTile;
+    [SerializeField] private GameObject _currentTile;
+
+    [SerializeField] private Vector3 _currentPosOnTile;
 
     private Vector3 _playerSartPos;
     private Quaternion _playerQuat;
@@ -48,7 +50,7 @@ public class TileSpawner : MonoBehaviour
     {
         GameObject tile = TilePool.Instance.GetPoolObj();
 
-        tile.transform.position = new Vector3(0F, 0F, _nextSpawnZ);
+        tile.transform.position = new Vector3(0f, 0f, _nextSpawnZ);
         tile.transform.rotation = Quaternion.identity;
         tile.SetActive(true);
 
@@ -60,54 +62,72 @@ public class TileSpawner : MonoBehaviour
     public void HideBackTile()
     {
         GameObject tileToHide = tiles[0];
-        tiles.RemoveAt(0);
-        TilePool.Instance.PutPoolObj(tileToHide);
+        tiles.RemoveAt(0); // RIMUOVO TILE
+        TilePool.Instance.PutPoolObj(tileToHide); // E LO RIMETTO NEL POOL
     }
 
     public void DestroyStartingTile()
     {
         if (_StartingTileGO != null)
         {
-            _StartingTileGO.SetActive(false);
+            _StartingTileGO.SetActive(false); // NON DISTRUGGO IL TILE MA LO NASCONDO
         }
+    }
+
+    private int GetCurrentTileIndex()
+    {
+        float playerZ = _player.position.z; // POSIZIONE ATTUALE DEL PLAYER
+
+        for (int i = tiles.Count - 1; i >= 0; i--)
+        {
+            if (playerZ >= tiles[i].transform.position.z)
+            {
+                return i;
+            }
+        }
+        return 0;
     }
 
     private void ResetRunningGame()
     {
-        //_currentTile = tiles[0];
-        //_startingTile = _currentTile;
+        if (tiles.Count == 0) return;
 
-        //for (int i = 1; i < tiles.Count; i++)
-        //{
-        //    TilePool.Instance.PutPoolObj(tiles[i]);
-        //}
+        int currentTileIndex = GetCurrentTileIndex();
+        _currentTile = tiles[currentTileIndex];
+        Debug.Log($"CURRENT TILE INDEX : {_currentTile}");
 
-        //tiles.Clear();
+        _currentPosOnTile = _player.position - _currentTile.transform.position; // MI SERVE PER EVITARE DI NOTARE LO STACCO DOPO IL TELEPORT
 
-        //_startingTile.transform.position = new Vector3(0f, 0f, 0f);
-        //_startingTile.transform.rotation = Quaternion.identity;
-        //_startingTile.SetActive(true);
+        float startZ = 0f;
+        if (_StartingTileGO != null)
+        {
+            startZ = _StartingTileGO.transform.position.z + _tileLength; // nuova posizione del tile dal punto appena dopo lo start tile
+        }
 
-        //_nextSpawnZ = _tileLength;
+        Vector3 currentPlayerPos = _player.position;
 
-        //Vector3 pos = _resetPos;
-        //Vector3 displacement = pos - _player.position;
+        tiles.RemoveAt(currentTileIndex); // RIMUOVO il tile DALLA LISTA dove si trova ora  il player
+        tiles.Insert(0, _currentTile);    // E PERò lo rimetto nella lista ma all'inizio della lista
+        float z = startZ;
+        for (int i = 0; i < tiles.Count; i++) // RISPAWNO ANCHE GLI ALTRI CHE ERO IN SEQUENZA DOPO IL TILE CORRENTE PER NON AVERE LO STACCO
+        {
+            tiles[i].transform.position = new Vector3(0f, 0f, z);
+            tiles[i].transform.rotation = Quaternion.identity;
+            z += _tileLength;
+        }
 
-        //_player.position = pos;
-        //_player.rotation = _playerQuat;
+        Vector3 newPlayerPos = _currentTile.transform.position + _currentPosOnTile;
+        _player.position = newPlayerPos;
+        _player.rotation = _playerQuat;
 
-        //if (_virtualcam != null)
-        //{
-        //    _virtualcam.OnTargetObjectWarped(_player, displacement);
-        //    _virtualcam.PreviousStateIsValid = false;
-        //}
+        Vector3 displacement = newPlayerPos - currentPlayerPos;
+        if (_virtualcam != null)
+        {
+            _virtualcam.OnTargetObjectWarped(_player, displacement);
+            _virtualcam.PreviousStateIsValid = false;
+        }
 
-        //while (tiles.Count < _initialNumTiles)
-        //{
-        //    SpawnTile();
-        //}
-
-        //isStartingGame = true;
+        _nextSpawnZ = tiles[tiles.Count - 1].transform.position.z + _tileLength;
     }
 
     private void Update()
