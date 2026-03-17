9e1c2fb0e041b9b1c3326d2c04c2eb8c4fa00370
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-03-17 20:27:45 +0100

    BIOMA 3 ADDED BLACK AND WHITE

---
+++ Assets/_Project/Scripts/Bioma/Bioma3FinalTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Bioma3FinalTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //TileSpawner.Instance.ChangeBioma();
+        }
+    }
+
+
+}
